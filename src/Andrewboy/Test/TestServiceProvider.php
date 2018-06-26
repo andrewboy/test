632ae2ae16539b948cb02ec0f89dea0bb527341b
Beck András <beck.andras@brightly.hu>
2018-06-26 10:50:24 +0200

    init test

---
+++ src/Andrewboy/Test/TestServiceProvider.php
@@ -0,0 +1,39 @@
+<?php namespace Andrewboy\SearchBox;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
